20b7ee83c383ceb1059ed3953b0da71f4101cf4f
rushan <rushan@thesanmark.com>
2024-09-23 15:44:25 +0530

    #3 Install tailwindCss and shadcnUi

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path"
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
